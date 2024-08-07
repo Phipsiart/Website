c234ff53dbe09fae5580636ee695f934f8449dd8
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2024-08-07 16:50:38 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-              <link rel="stylesheet" href="https://cdn.phipsiart.at/leberkasrechner-assets/css/tabler.min.css" />
-              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
-      <script src='https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js' defer></script>
-      <Head />
-      <body className='theme-codensed theme-light'>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.phipsiart.at/leberkasrechner-assets/css/tabler.min.css" />
+        <script 
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+          crossOrigin="anonymous">
+        </script>    
+        <script 
+          src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js" 
+          defer>
+        </script>
+      </Head>
+      <body className="theme-condensed theme-light">
         <Main />
         <NextScript />
       </body>

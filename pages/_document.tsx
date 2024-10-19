aec0ed30ac431eae4a877f4866868d34101ee2b6
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2024-10-19 10:06:30 +0200

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html lang="en">
@@ -7,8 +6,7 @@ export default function Document() {
         <link rel="stylesheet" href="https://cdn.phipsiart.de/leberkasrechner-assets/css/tabler.min.css" />
         <script 
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
-          crossOrigin="anonymous">
+          defer>
         </script>    
         <script 
           src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js" 
